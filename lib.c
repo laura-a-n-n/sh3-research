b59c21f07a87d35817719bc17e711d3aaaed4543
laura ann <laura@lauraann.dev>
2025-11-22 10:45:51 -0800

    more c & notes

--- lib.c
+++ lib.c
@@ -8,9 +8,15 @@ int func_00153830(void *mdlHeader, int size)
   }
   else {
     for (i = 0; i < size; i = i + 0x10) {
-      *((char *)mdlHeader + i) = 0x12;
+      *((char *)mdlHeader + i) = ((i % 8) * 0x11) + 0x12;
     }
   }
   return size;
+}
+
+void func_011fd28(int* arg0, int arg1) {
+    arg0[0] = arg1;
+    arg0[2] = 0;
+    arg0[1] = arg1;
 }
 
