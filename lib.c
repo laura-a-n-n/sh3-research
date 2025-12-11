28614ad6da7f8e8ce1019e031e123c711b29593a
laura ann <laura@lauraann.dev>
2025-12-11 02:27:36 -0800

    add `func_002A8560`

--- lib.c
+++ lib.c
@@ -1,3 +1,7 @@
+typedef unsigned char u8;
+typedef signed char s8;
+typedef int s32;
+
 // 0x00153830 - fills chhaa.mdl region with 0x12
 int func_00153830(void *mdlHeader, int size)
 {
@@ -20,3 +24,38 @@ void func_011fd28(int* arg0, int arg1) {
     arg0[1] = arg1;
 }
 
+extern u8 D_00398310[256];
+extern s8 D_01F605B0[256];
+void func_002A86E0(u8* input, s32 index);
+
+// 0x002A8560 - costume code checker
+s32 func_002A8560() {
+    u8 input[16];
+    s32 codeIndex = 0;
+    s32 codeOffset = 0;
+
+    while (1) {
+        u8 codeLength = *(u8* )0x01F605D0;
+        s32 i = 0;
+        for (i = 0; i < codeLength; i++) {
+            input[i] = D_01F605B0[i];
+        }
+        for (i = codeLength; i < 0x10; i++) {
+            input[i] = 0;
+        }
+        func_002A86E0(input, codeIndex);
+        for (i = 0; i < 0x10; i++) {
+            if (D_00398310[i + codeOffset] != input[i]) {
+                break;
+            }
+        }
+        if (i == 0x10) {
+            return codeIndex;
+        }
+        codeIndex++;
+        codeOffset += 0x10;
+        if (codeIndex >= 0x1A) {
+            return -1;
+        }
+    }
+}
